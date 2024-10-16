8eca7977b663db5670c257f7106fd6ed28a4e6f3
PatoComDengue <116114315+Joaoheheha@users.noreply.github.com>
2024-10-16 10:53:35 -0300

    Adicionano mais perguntas!

--- pergunta.js
+++ pergunta.js
@@ -11,7 +11,19 @@ criaCartao (
 );
 
 criaCartao (
-   "programação",
+   "Programação",
    "O que são o Java script e o C++",
    "São linguagens de programação"
 );
+
+criaCartao (
+    "Piadas infadonhas",
+    "Como você deixa um carteiro triste?",
+    "Você mata a família dele!"
+ );
+
+ criaCartao (
+    "Programação",
+    "Para que serve a tag P?",
+    "Para criar um parágrafo!"
+ )
